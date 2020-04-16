77a95c61a369bb2abed1941da216264ac479f225
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-04-16 12:34:34 +0100

    add game summary

--- src/GameSummary.js
+++ src/GameSummary.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const GameSummary = () => {
+const GameSummary = (props) => {
+  const { score, questionCount } = props.location.state;
+  const answerRate = 100 * (score / questionCount).toFixed(2);
   return (
     <>
-      <div>You had (nned to pass number of questions)</div>
-      <div>You guessed (need to pass number of correct guesses)</div>
-      <div>Your answer rate ()</div>
+      <div>You had {questionCount} questions</div>
+      <div>You guessed {score} flags</div>
+      <div>You guessed correctly {answerRate}% of flags</div>
+      <Link to='/game-select'>New Game?</Link>
     </>
   );
 };
