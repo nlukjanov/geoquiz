2bd91049ef6941feb92ea7de97035944fdedb109
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2021-01-06 09:44:15 +0000

    chore: add commit lint and lint staged

--- src/GameSelect.js
+++ src/GameSelect.js
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom';
 const GameSelect = () => {
   return (
     <div className='game-select' data-testid='game-select'>
-      <Link data-testid='world' to='/world'><button>World</button></Link>
-      <Link data-testid='europe' to='/europe'><button>Europe</button></Link>
-      <Link data-testid='asia' to='/asia'><button>Asia</button></Link>
-      <Link data-testid='africa' to='/africa'><button>Africa</button></Link>
-      <Link data-testid='americas' to='/americas'><button>Americas</button></Link>
+      <Link data-testid='world' to='/world'>
+        <button>World</button>
+      </Link>
+      <Link data-testid='europe' to='/europe'>
+        <button>Europe</button>
+      </Link>
+      <Link data-testid='asia' to='/asia'>
+        <button>Asia</button>
+      </Link>
+      <Link data-testid='africa' to='/africa'>
+        <button>Africa</button>
+      </Link>
+      <Link data-testid='americas' to='/americas'>
+        <button>Americas</button>
+      </Link>
     </div>
   );
 };
